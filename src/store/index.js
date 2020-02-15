90b03df38b5cf9f97379c998c76873151be92716
Jama Mohamed <jama3137@gmail.com>
2020-02-15 21:22:20 +0300

    [Started] Set up the store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    city: "Nairobi",
+    weatherDescription: "broken clouds",
+    weatherIcon: "cloud-showers-heavy",
+    temperature: 12,
+    windSpeed: 50,
+    queryCity: ""
   },
   mutations: {
+
+  },
+  getters: {
+    getTemperature: state => {
+      return state.temperature
+    }
   },
   actions: {
+    fetchWeather: () => {
+      
+    }
   },
-  modules: {
-  }
 })
