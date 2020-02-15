b90e00e0816abbf5e3c4b21cf93a1a96d1a8df33
Jama Mohamed <jama3137@gmail.com>
2020-02-15 13:55:59 +0300

    [Started] Added the components to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-        Home
-    </div>
+  <div>
+      <Header />
+      <WeatherIcon />
+      <Details />
+      <Input />
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import Header from "../components/Header"
+import WeatherIcon from "../components/WeatherIcon"
+import Details from "../components/Details"
+import Input from "../components/Input"
+
+export default {
+    components: {
+        Header,
+        WeatherIcon,
+        Details,
+        Input
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
