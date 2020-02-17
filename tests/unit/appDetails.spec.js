2a3c78d8e80b9672f31654007a199c3c0d793a8b
Jama Mohamed <jama3137@gmail.com>
2020-02-17 11:17:58 +0300

    [Finished] Remove some boiler plate from tests

--- tests/unit/appDetails.spec.js
+++ tests/unit/appDetails.spec.js
@@ -6,30 +6,22 @@ import store from "@/store"
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
+const stubs = ["b-row", "b-col", "font-awesome-icon"]
+
 describe('AppDetails', () => {
 
   it("render wind speed", () => {
 
     store.state.weatherData.windSpeed = 24
     const wrapper = shallowMount(AppDetails, {
-      store, localVue, stubs: {
-        "b-row": true,
-        "b-col": true,
-        "font-awesome-icon": true
-      }
-    })
+      store, localVue, stubs })
     expect(wrapper.findAll("p").at(1).text()).toBe("24 m/s")
   })
 
   it("render weather temperature from getter", () => {
     store.state.weatherData.temperature = 299.65
     const wrapper = shallowMount(AppDetails, {
-      store, localVue, stubs: {
-        "b-row": true,
-        "b-col": true,
-        "font-awesome-icon": true
-      }
-    })
+      store, localVue, stubs })
     expect(wrapper.find("p").text()).toBe("27 °")
   })
 
